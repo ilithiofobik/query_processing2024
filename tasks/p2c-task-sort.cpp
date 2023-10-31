3d684acaec6b8892b3dcb8d5d370ed616eae1fd7
Gabriel Haas <gabriel.haas@tum.de>
2023-10-31 15:16:22 +0000

    sort with distinct result and limit

--- tasks/p2c-task-sort.cpp
+++ tasks/p2c-task-sort.cpp
@@ -24,11 +24,12 @@ int main()
    {
       auto l = make_unique<Scan>("lineitem");
       IU* lq = l->getIU("l_quantity");
-      IU* lls = l->getIU("l_linestatus");
+      IU* lp = l->getIU("l_extendedprice");
       IU* lo = l->getIU("l_orderkey");
+      IU* lc = l->getIU("l_comment");
 
-      auto sort = make_unique<Sort>(std::move(l), std::vector<IU*>({lq, lls}));
-      produceAndPrint(std::move(sort), {lo, lq, lls}, 2, 99999, 10);
+      auto sort = make_unique<Sort>(std::move(l), std::vector<IU*>({lq, lp, lo}));
+      produceAndPrint(std::move(sort), {lq, lp, lo}, 2, 59986043, 10);
    }
    return 0;
 }
