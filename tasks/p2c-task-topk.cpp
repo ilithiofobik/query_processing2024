c73280c26d9a356ac8fa4f70716ea56fe003d98f
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-12-20 10:47:34 +0100

    tol init

--- tasks/p2c-task-topk.cpp
+++ tasks/p2c-task-topk.cpp
@@ -1,35 +1,39 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <unordered_map>
-#include <vector>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+
+#include <iostream>
+#include <map>
 #include <source_location>
+#include <sstream>
 #include <string>
 #include <string_view>
-#include <sstream>
+#include <unordered_map>
+#include <vector>
 
-#include "p2c/foundations.hpp"
-#include "p2c/types.hpp"
-#include "p2c/tpch.hpp"
 #include "additionaloperators.hpp"
+#include "p2c/foundations.hpp"
+#include "p2c/tpch.hpp"
+#include "p2c/types.hpp"
 #include "paralleloperators.hpp"
+#include "tree_of_losers.hpp"
 
 using namespace std;
 using namespace fmt;
 using namespace p2c;
 
-int main()
-{
-   {
-      auto l = make_unique<ParallelScan>("lineitem");
-      IU* lq = l->getIU("l_quantity");
-      IU* lp = l->getIU("l_extendedprice");
-      IU* lo = l->getIU("l_orderkey");
+int main() {
+    {
+        auto l = make_unique<ParallelScan>("lineitem");
+        IU* lq = l->getIU("l_quantity");
+        IU* lp = l->getIU("l_extendedprice");
+        IU* lo = l->getIU("l_orderkey");
 
-      auto topK = make_unique<ParallelTopK>(std::move(l), std::vector<IU*>({lq, lp, lo}), 10);
-      produceAndSynchronizedPrint(std::move(topK), {lq, lp, lo}); // limits the ouptut (don't use this for optimization)
-   }
-   return 0;
+        auto topK = make_unique<ParallelTopK>(
+            std::move(l), std::vector<IU*>({lq, lp, lo}), 10);
+        produceAndSynchronizedPrint(
+            std::move(topK),
+            {lq, lp,
+             lo});  // limits the ouptut (don't use this for optimization)
+    }
+    return 0;
 }
